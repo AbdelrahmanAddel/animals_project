e03c27be5e879c5e33407ea6b44b0d9a2a8f0809
Abdelrahman <Aa5282243@gmail.com>
2025-10-15 23:02:13 +0300

    Add onboarding images and font

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class images {
+  images._();
+  
+  /// Assets for pngOnboardingImage
+  /// assets/png/onboarding_image.png
+  static const String pngOnboardingImage = "assets/png/onboarding_image.png";
+
+  /// Assets for pngSplashImage
+  /// assets/png/splashImage.png
+  static const String pngSplashImage = "assets/png/splashImage.png";
+
+  /// Assets for svgPetsFoot
+  /// assets/svg/petsFoot.svg
+  static const String svgPetsFoot = "assets/svg/petsFoot.svg";
+}
+
