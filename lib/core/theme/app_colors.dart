142791fed6f5ef590cf319f7531f3f007413840c
Abdelrahman <Aa5282243@gmail.com>
2025-10-16 15:09:37 +0300

    Finish  Home Cubit And Handle different States

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff44BDB6);
+  static const Color textColor = Color(0xff090909);
+  static const Color greyColor = Color(0xff646464);
+}
---
+++ lib/core/theme/app_text_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:animal_task/core/theme/app_colors.dart';
+
+class AppTextStyle {
+  static TextStyle poppins700Bold24 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    color: AppColors.textColor,
+  );
+  static TextStyle poppins400Regular14 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    color: AppColors.greyColor,
+  );
+}
